67d66001fb969bae3e3b1463282edc6758b84ba6
maheshvara75 <73717752+maheshvara75@users.noreply.github.com>
2024-06-17 17:15:59 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,11 +1,11 @@
 variable "aws_access_key" {
-  default ="ASIA6CNX3H6YQ4Z4ZSP5"
+  default =""
   }
 variable "aws_secret_key" {
-  default ="gwQiT910Jfysx6IPyn6+VSlIG7LbifoH+oWQ13Xa"
+  default =""
   }
 variable "aws_session_token" {
-default="FwoGZXIvYXdzEFsaDNVdMNqQF1HLPQqcHyK3ASTbCZZZso3SOwn2UHCQMKZzvnk9YkAxE0UNrG37k6IieGDvE4xUPzroh2T9pM8pyc5zHmxCejLmPx89BoGKgFtsFAY0cgTloNkhcc6/Dy5OSsJHbIlB/fYiPN4+il2jWfXEizpaYk93pgokd23taN60vdDQcVJDiVL5tDAd5ZJM9dPXpUy65JLnl3nHfPlvt2Yjg+3+jFMyiqY2G/lHAJC4bcMpfNbGlKjzGd4+rqma+0vNLuioMCikob6zBjIt902XndYEIwRWzgJm7V/PnkARwdd2OmIRmizAbFHa61ow77LHFwbpALbYMzFv"
+default=""
   }
 variable "key_name" {
   default ="ec2pro2_pem"
