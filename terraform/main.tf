a54160f100414cb405684a60c631e3c42f2f14a9
maheshvara75 <73717752+maheshvara75@users.noreply.github.com>
2024-06-18 09:49:31 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -14,23 +14,6 @@ provider "aws" {
   token      = var.aws_session_token
 }
 
-resource "aws_security_group" "allow_ssh" {
-  name        = "allow_ssh"
-  description = "Allow SSH inbound traffic"
-  
-  ingress {
-    description = "SSH"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "allow_ssh"
-  }
-}
-
 # RSA key of size 4096 bits
 resource "tls_private_key" "rsa_4096" {
   algorithm = "RSA"
