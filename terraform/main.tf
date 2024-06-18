90de8fb33f29d3b934e58f531bfcb2b9b633596f
maheshvara75 <73717752+maheshvara75@users.noreply.github.com>
2024-06-18 09:59:04 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,7 +6,22 @@ terraform {
     }
   }
 }
+resource "aws_security_group" "allow_ssh" {
+  name        = "allow_ssh"
+  description = "Allow SSH inbound traffic"
+  
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
+  tags = {
+    Name = "allow_ssh"
+  }
+}
 provider "aws" {
   region  = "us-east-1"
   access_key = var.aws_access_key
