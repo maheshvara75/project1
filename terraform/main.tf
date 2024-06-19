e457e250b3dcda2466fc2971636c1dd76beadf46
maheshvara75 <73717752+maheshvara75@users.noreply.github.com>
2024-06-19 07:44:30 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -42,6 +42,12 @@ resource "aws_security_group" "allow_ssh" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
   tags = {
     Name = "allow_ssh"
