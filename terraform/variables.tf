6137652a02083c39b2ecdd956f65b906007abcfc
maheshvara75 <73717752+maheshvara75@users.noreply.github.com>
2024-06-19 08:15:18 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -21,10 +21,10 @@ variable "key_name" {
 variable "instance_ami" {
   description = "AMI ID for the EC2 instances"
   type        = string
-  default     = "ami-04b70fa74e45c3917"
+  default     = ""
 }
 variable "instance_type" {
   description = "Instance type for the EC2 instances"
   type        = string
-  default     = "t2.micro"
+  default     = ""
 }
